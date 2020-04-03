dde54cb50eebcc26fe850c080226cb26d14c7345
RKLogixal <rohit.karkhanis@logixal.com>
2020-04-03 14:49:30 +0530

    Changed local folder , added customised email code , other changes

---
+++ src/test/java/com/testcases/driverscripts/TestJar.java
@@ -0,0 +1,14 @@
+package com.testcases.driverscripts;
+
+import JarPack.ExecuteJarTestNG;
+
+public class TestJar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ExecuteJarTestNG tg = new ExecuteJarTestNG();
+		tg.ExecuteSuite();
+	}
+
+}
